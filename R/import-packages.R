1ac90c8844d42a10c1c07f411a8356da06f70ef5
lihualei71 <lihua.lei@berkeley.edu>
2019-01-16 23:13:45 -0800

    V0.1.0 completed

---
+++ R/import-packages.R
@@ -0,0 +1,5 @@
+#' @importFrom exactci exactbinomCI
+#' @importFrom methods formalArgs
+#' @importFrom stats binomial coef ecdf glm median model.matrix optimize predict sd rnorm runif
+#' @importFrom utils setTxtProgressBar txtProgressBar
+NULL
--- R/main.R
+++ R/main.R
@@ -1,3 +1,57 @@
+#' O-values of Observational Studies
+#'
+#' \code{ovalue} is a framework to calculate O-values of observational studies allowing for arbitrary classifiers to calculate 1-d scores.
+#'
+#' @details \code{ovalue} provides a bunch of user-friendly wrappers and a flexible framework that allows for arbitrary classifiers. For the former, \code{scorefun} can be a valid string, including
+#' * "logistic" for logistic regression,
+#' * "lasso" for L1-penalized logistic regression,
+#' * "gam" for generalized additive model,
+#' * "gbm" for generalized boosting machine,
+#' * "rf" for random forest.
+#' 
+#' For the latter, \code{scorefun} can be a function object whose inputs must include
+#' * "T" for treatment vector, must be a logical vector or a factor/vector encoded by 0 and 1,
+#' * "X" for covariates, must be a vector/matrix/data.frame,
+#' * "trainid" for the index of training samples, must be a logical vector or a vector of integers.
+#'
+#' \code{ovalue} supports two types of data inputs: (1) \code{T} and \code{X} or (2) \code{formula} and \code{data}. One of the pair has to be specified.  
+#' @md
+#'
+#' @param T a logical vector or a factor/vector encoded by 0 and 1. Treatment assignment
+#' @param X a matrix or a data.frame. Covariate
+#' @param formula a formula object. See Details
+#' @param data a data.frame. See Details
+#' @param alpha numeric. Confidence level
+#' @param scorefun a string or a function. See Details
+#' @param trainprop numeric. Proportion of training samples
+#' @param nreps an integer. Number of times for data splitting
+#' @param verbose logical. Indicate whether relevant information is outputted to the console
+#' @param ... Other arguments passed into \code{scorefun}
+#'
+#' @return
+#' \item{eta}{ median of O-values for all splitted data.}
+#' \item{etalist}{ list of O-values for all splitted data.}
+#' 
+#' @examples
+#' \donttest{# Generate data from a logistic model
+#' set.seed(1)
+#' n <- 1000
+#' p <- 50
+#' X <- matrix(stats::rnorm(n * p), n, p)
+#' beta <- rep(1 / sqrt(p), p)
+#' probs <- 1 / (1 + exp(-X %*% beta))
+#' T <- stats::runif(n) <= probs
+#' data <- data.frame(T = T, X)
+#'
+#' # Calculate the O-value with inputs \code{T} and \code{X}
+#' set.seed(1)
+#' ovalue(T, X, scorefun = "gbm")
+#'
+#' # Calculate the O-value with inputs \code{formula} and \code{data}
+#' set.seed(1)
+#' ovalue(formula = T ~ ., data = data, scorefun = "gbm")
+#' }
+#' 
 #' @export
 ovalue <- function(T = NULL, X = NULL,
                    formula = NULL, data = NULL,
