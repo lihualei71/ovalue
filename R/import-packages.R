aa325b9c253cd564f52ecd93b911f53847fc2608
lihualei71 <lihua.lei@berkeley.edu>
2019-02-19 16:41:01 -0800

    add references for lalonde_dw

--- R/import-packages.R
+++ R/import-packages.R
@@ -2,4 +2,5 @@
 #' @importFrom methods formalArgs
 #' @importFrom stats binomial coef ecdf glm median model.matrix optimize predict quantile sd rnorm runif
 #' @importFrom utils setTxtProgressBar txtProgressBar
+#' @importFrom Rdpack reprompt
 NULL
--- R/lalonde_dw.R
+++ R/lalonde_dw.R
@@ -4,4 +4,9 @@
 #'
 #' Each entry is a data.frame with name "cps", "cps2", "cps3", "psid", "psid2", "psid3" and "dw". "dw" corresponds to the dataset from the RCT with 185 treated and 260 control units (with earnings in 1974 available). The other datasets append the corresponding external controls to the dataset given by "dw". See \url{http://users.nber.org/~rdehejia/data/nswdata2.html} for details. 
 #'
+#' @references
+#' \insertRef{lalonde1986evaluating}{ovalue}
+#'
+#' \insertRef{dehejia1999causal}{ovalue}
+#' 
 "lalonde_dw"
